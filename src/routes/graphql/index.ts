b68e6fd0e54ff07c668a55ee56edb13b33a126c8
alami <alami@km.ru>
2023-05-29 11:31:38 +0500

    upd schema

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,5 +1,7 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
 import { graphqlBodySchema } from './schema';
+import {buildSchema, graphql} from "graphql";
+
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
@@ -11,7 +13,32 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         body: graphqlBodySchema,
       },
     },
-    async function (request, reply) {}
+    async function (request, reply) {
+      const source = request.body.query!
+      const schema = buildSchema(`
+                          type Query {
+                              profiles: [Profiles]
+                              posts: [Posts]
+                              users: [Users]
+                              memberTypes: [MemberTypes]
+                          }
+                          `);
+      const rootValue = {
+        profiles: () => {
+          return this.db.profiles.findMany();
+        },
+        posts: () => {
+          return this.db.posts.findMany();
+        },
+        users: () => {
+          return this.db.users.findMany();
+        },
+        memberTypes: () => {
+          return this.db.memberTypes.findMany();
+        },
+      }
+      return graphql({schema, source, rootValue})
+    }
   );
 };
 
